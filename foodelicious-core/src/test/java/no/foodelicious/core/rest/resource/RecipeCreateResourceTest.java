088310917ed535e33c866069f418880304832978
Marius Breivik <marius.breivik@gmail.com>
2013-09-24 22:12:41 +0200

    Added test for recipe creation

--- foodelicious-core/src/test/java/no/foodelicious/core/rest/resource/RecipeCreateResourceTest.java
+++ foodelicious-core/src/test/java/no/foodelicious/core/rest/resource/RecipeCreateResourceTest.java
@@ -1,11 +1,24 @@
 package no.foodelicious.core.rest.resource;
 
+import org.junit.Test;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import no.foodelicious.service.Recipe;
+import no.foodelicious.service.RecipeCreateResource;
+
 import com.yammer.dropwizard.testing.ResourceTest;
 
 public class RecipeCreateResourceTest extends ResourceTest{
 
 	@Override
 	protected void setUpResources() throws Exception {
+        addResource(new RecipeCreateResource());
+	}
+	
+	@Test
+	public void testCreateRecipe(){
+		Recipe recipe = new Recipe(99L);
+		assertThat(client().resource("/recipe/create/").post(Recipe.class).equals(recipe));
 		
 	}
 }
