65afdefe292e65c725fd5484193dbf4736138c30
Marius Breivik <marius.breivik@gmail.com>
2013-09-24 21:12:28 +0200

    Added dropwizard-testing

---
+++ foodelicious-core/src/test/java/no/foodelicious/core/rest/resource/RecipeCreateResourceTest.java
@@ -0,0 +1,11 @@
+package no.foodelicious.core.rest.resource;
+
+import com.yammer.dropwizard.testing.ResourceTest;
+
+public class RecipeCreateResourceTest extends ResourceTest{
+
+	@Override
+	protected void setUpResources() throws Exception {
+		
+	}
+}
