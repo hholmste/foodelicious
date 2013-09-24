d7ce28ddee0c76aeb36e94d94a097934508a01a1
Morten Berg <mortenberg80@gmail.com>
2013-09-24 22:35:16 +0200

    Fixed test so that it correctly sends JSON-data to the resource

--- foodelicious-core/src/test/java/no/foodelicious/core/rest/resource/RecipeCreateResourceTest.java
+++ foodelicious-core/src/test/java/no/foodelicious/core/rest/resource/RecipeCreateResourceTest.java
@@ -1,24 +1,25 @@
 package no.foodelicious.core.rest.resource;
 
+import javax.ws.rs.core.MediaType;
+
 import org.junit.Test;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import no.foodelicious.service.Recipe;
-import no.foodelicious.service.RecipeCreateResource;
+import no.foodelicious.service.RecipeResource;
 
 import com.yammer.dropwizard.testing.ResourceTest;
 
-public class RecipeCreateResourceTest extends ResourceTest{
+public class RecipeCreateResourceTest extends ResourceTest {
 
-	@Override
-	protected void setUpResources() throws Exception {
-        addResource(new RecipeCreateResource());
-	}
-	
-	@Test
-	public void testCreateRecipe(){
-		Recipe recipe = new Recipe(99L);
-		assertThat(client().resource("/recipe/create/").post(Recipe.class).equals(recipe));
-		
-	}
+    @Override
+    protected void setUpResources() throws Exception {
+        addResource(new RecipeResource());
+    }
+
+    @Test
+    public void testCreateRecipe() {
+        Recipe recipe = new Recipe(99L);
+        assertThat(client().resource("/recipe").type(MediaType.APPLICATION_JSON).post(Recipe.class, recipe).equals(recipe));
+    }
 }
