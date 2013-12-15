5ac57223da56b4333af6e96645f03edcfdc5ada8
Marius Breivik <marius.breivik@gmail.com>
2013-12-15 22:30:30 +0100

    Refactored front-end

--- src/main/resources/assets/js/module.js
+++ src/main/resources/assets/js/module.js
@@ -1,8 +1,8 @@
-var recipeModule = angular.module('recipeApp', ['ngRoute']);
-
-recipeModule.config(function($locationProvider,$routeProvider){
-  $routeProvider
-    .when("/index.html", {templateUrl: "/views/partials/recipe_overview.html"}
-    .when("/new", {templateUrl: "/views/partials/recipe_detail.html"})
-  );
+var recipeModule = angular.module('recipeApp', ['ngRoute'])
+	.config(function($locationProvider,$routeProvider){
+		  $routeProvider
+		    .when("/index.html", {templateUrl: "/views/partials/recipe_overview.html"})
+		    .when("/new", {templateUrl: "/views/partials/recipe_detail.html"})
+          $locationProvider.html5Mode(true)
+		  
 });
