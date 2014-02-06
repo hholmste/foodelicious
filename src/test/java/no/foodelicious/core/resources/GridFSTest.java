8555204d94e1713b59451d918b442086306a4712
Morten Berg <mortenberg80@gmail.com>
2014-02-06 16:05:06 +0100

    La til enkel test for GridFS for å finne ut hvordan APIet fungerer. Ignorert, da den krever en kjørende mongodb.

---
+++ src/test/java/no/foodelicious/core/resources/GridFSTest.java
@@ -0,0 +1,44 @@
+package no.foodelicious.core.resources;
+
+import java.io.File;
+import java.util.List;
+
+import no.foodelicious.core.configuration.MongoConfiguration;
+import no.foodelicious.core.factory.RepositoryFactory;
+
+import org.bson.types.ObjectId;
+import org.fest.assertions.api.Assertions;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSInputFile;
+
+/**
+ * Test made to explore the GridFS api. Requires a running mongodatabase at 127.0.0.1:27017
+ */
+public class GridFSTest {
+//	private static final Logger LOG = LoggerFactory.getLogger(GridFSTest.class);
+	GridFS gridFs = new RepositoryFactory().createGridFS(new MongoConfiguration("127.0.0.1", 27017));
+
+	@Test
+	@Ignore
+	public void testInsertAndRemove() throws Exception {
+		File testImg = new File("src/test/resources/test.png");
+
+		GridFSInputFile inputFile = gridFs.createFile(testImg);
+		inputFile.save();
+//		LOG.debug(String.format("Inserted file [id=%s]", inputFile.getId()));
+//		DBCursor fileList = gridFs.getFileList();
+//		for (DBObject dbObject : fileList) {
+//			LOG.debug(String.format("File [dbObject=%s]", dbObject.toString()));
+//		}
+		List<GridFSDBFile> afterInsert = gridFs.find(inputFile);
+		Assertions.assertThat(afterInsert).isNotEmpty();
+
+		gridFs.remove(new ObjectId(inputFile.getId().toString()));
+		List<GridFSDBFile> afterRemove = gridFs.find(inputFile);
+		Assertions.assertThat(afterRemove).isEmpty();
+	}
+}
