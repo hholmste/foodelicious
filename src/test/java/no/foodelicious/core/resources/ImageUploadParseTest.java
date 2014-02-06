a20e1e26006936acd17989bcf163ded4819804b9
Morten Berg <mortenberg80@gmail.com>
2014-02-06 16:12:07 +0100

    Fjernet ubrukte imports

--- src/test/java/no/foodelicious/core/resources/ImageUploadParseTest.java
+++ src/test/java/no/foodelicious/core/resources/ImageUploadParseTest.java
@@ -1,21 +1,15 @@
 package no.foodelicious.core.resources;
 
 import static com.yammer.dropwizard.testing.JsonHelpers.asJson;
-import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-import no.foodelicious.core.model.Recipe;
-
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class ImageUploadParseTest {
-	
 	
 	@Test
 	@Ignore
