495cdf4cdc9c28ab9cebd76e0866fbf8b20d624e
Morten Berg <mortenberg80@gmail.com>
2014-02-06 16:06:06 +0100

    Fikset bildeopplastingendepunktet til å lagre bilder til GridFS. Også lagt ved en test som mocker ut GridFS.

--- src/main/java/no/foodelicious/core/resources/ImageResource.java
+++ src/main/java/no/foodelicious/core/resources/ImageResource.java
@@ -1,53 +1,49 @@
 package no.foodelicious.core.resources;
 
+
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.bson.types.ObjectId;
-
-import com.google.common.base.Optional;
-import com.mongodb.BasicDBObject;
-import com.mongodb.QueryBuilder;
 import com.mongodb.gridfs.GridFS;
-import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSInputFile;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 @Path("/image")
-@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class ImageResource {
 
-	private GridFS gridFs;
+	private final GridFS gridFs;
 
 	public ImageResource(GridFS gridFs) {
 		this.gridFs = gridFs;
 	}
 
 	@POST
-	public Response uploadImage(InputStream inputStream) {
-		GridFSInputFile file = gridFs.createFile(inputStream);
-		return Response.ok(file.getId().toString()).build();
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response image(@FormDataParam("file") InputStream inputStream, @FormDataParam("file") FormDataContentDisposition fileDetail) {
+		try {
+			GridFSInputFile file = gridFs.createFile(inputStream, fileDetail.getFileName());
+			file.save();
+			return Response.ok(file.getId().toString()).build();
+		} catch (Exception e) {
+			return Response.serverError().build();
+		}
+
 	}
 
-	@GET
-	@Path("/{id}")
-	public Response findById(@PathParam("id") final String id) throws Exception {
-		GridFSDBFile gridFSDBFile = gridFs.findOne(new ObjectId(id));
-
-		
-		ImageStreamingOutput out = new ImageStreamingOutput();
-		
-		return Response.ok().build();
-	}
+//	@GET
+//	@Path("/{id}")
+//	@Consumes
+//	public Response findById(@PathParam("id") final String id) throws Exception {
+//		GridFSDBFile gridFSDBFile = gridFs.findOne(new ObjectId(id));
+//		
+//		ImageStreamingOutput out = new ImageStreamingOutput();
+//		
+//		return Response.ok().build();
+//	}
 }
--- src/test/java/no/foodelicious/core/resources/ImageUploadResourceTest.java
+++ src/test/java/no/foodelicious/core/resources/ImageUploadResourceTest.java
@@ -1,33 +1,71 @@
 package no.foodelicious.core.resources;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 import javax.ws.rs.core.MediaType;
 
-import no.foodelicious.core.configuration.MongoConfiguration;
-import no.foodelicious.core.factory.RepositoryFactory;
-
-import org.junit.Ignore;
+import org.fest.assertions.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.mongodb.MongoException;
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSInputFile;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.multipart.FormDataMultiPart;
+import com.sun.jersey.multipart.file.FileDataBodyPart;
 import com.yammer.dropwizard.testing.ResourceTest;
 
-public class ImageUploadResourceTest extends ResourceTest{
+@RunWith(MockitoJUnitRunner.class)
+public class ImageUploadResourceTest extends ResourceTest {
+
+	@Mock
+	private GridFS mockGridFs;
+
+	@Mock
+	private GridFSInputFile mockGridFSInputFile;
+
+	@Before
+	public void setup() {
+		when(mockGridFs.createFile(any(InputStream.class), anyString())).thenReturn(mockGridFSInputFile);
+		when(mockGridFSInputFile.getId()).thenReturn("id");
+	}
 
 	@Override
 	protected void setUpResources() throws Exception {
-		addResource(new ImageResource(new RepositoryFactory().createGridFS(new MongoConfiguration())));
+		addResource(new ImageResource(mockGridFs));
 	}
-	
+
 	@Test
-	@Ignore
-	public void testImageUploadResource() throws Exception{
+	public void testImageUploadResourceOk() throws Exception{
+		final FormDataMultiPart multipart = createRequest();
+
+		ClientResponse postResponse = client().resource("/image").type(MediaType.MULTIPART_FORM_DATA).post(ClientResponse.class, multipart);
+		Assertions.assertThat(postResponse.getStatus()).isEqualTo(200);
+	}
+
+	@Test
+	public void testImageUploadResourceFailed() throws Exception{
+		final FormDataMultiPart multipart = createRequest();
+		doThrow(new MongoException("foo")).when(mockGridFSInputFile).save();
 		
-		File testImg = new File("src/test/resources/sushi.JPG");
-		InputStream inputStream = new FileInputStream(testImg);
-		
-		client().resource("/imageupload").type(MediaType.MULTIPART_FORM_DATA).post(InputStream.class,inputStream);
+		ClientResponse postResponse = client().resource("/image").type(MediaType.MULTIPART_FORM_DATA).post(ClientResponse.class, multipart);
+		Assertions.assertThat(postResponse.getStatus()).isEqualTo(500);
+	}
+
+	private FormDataMultiPart createRequest() {
+		final File testImg = new File("src/test/resources/test.png");
+		final FileDataBodyPart filePart = new FileDataBodyPart("file", testImg);
+		final FormDataMultiPart multipart = new FormDataMultiPart();
+		multipart.bodyPart(filePart);
+		return multipart;
 	}
 }
