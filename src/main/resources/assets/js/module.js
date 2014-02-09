3e47cce00fbd03f2eef411ed04498701530c88a6
Morten Berg <mortenberg80@gmail.com>
2014-02-09 21:49:44 +0100

    Fixed routing. Removed image cloud in recipe_overview.html, replaced with dump text.

--- src/main/resources/assets/js/module.js
+++ src/main/resources/assets/js/module.js
@@ -1,8 +1,17 @@
-var recipeModule = angular.module('recipeApp', ['ngRoute'])
-	.config(function($locationProvider,$routeProvider){
-		  $routeProvider
-		    .when("/index.html", {templateUrl: "/views/partials/recipe_overview.html"})
-		    .when("/new", {templateUrl: "/views/partials/recipe_detail.html"})
-          $locationProvider.html5Mode(true)
-		  
-});
+var recipeModule = angular.module('recipeApp', [
+	'ngRoute'
+]);
+
+recipeModule.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider
+			.when("/recipes", {
+				templateUrl: "/views/partials/recipe_overview.html"
+			})
+			.when("/new", {
+				templateUrl: "/views/partials/recipe_detail.html"
+			})
+			.otherwise({
+				redirectTo: '/recipes'
+			});
+	}]);
