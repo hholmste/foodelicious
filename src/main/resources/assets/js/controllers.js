809b8c17a2b42f4d543f75af6cb2489249213e9c
Morten Berg <mortenberg80@gmail.com>
2014-02-10 21:32:38 +0100

    Recipes are retrieved from the recipe-REST-endpoint.

--- src/main/resources/assets/js/controllers.js
+++ src/main/resources/assets/js/controllers.js
@@ -1,10 +1,18 @@
 var recipeControllers = angular.module('recipeControllers', []);
 
-recipeControllers.controller('RecipeCtrl', function($scope) {
-	$scope.recipes = [
-		{'name': 'receipname1',
-		 'description': 'receipdescription1'},
-		{'name': 'receipname2',
-		 'description': 'receipdescription2'},
-	];
-});
+recipeControllers.controller('RecipeCtrl', ['$scope', '$http',
+	function($scope, $http) {
+		$http.get('http://localhost:8080/foodelicious/recipe').
+		success(function(data) {
+			$scope.recipes = data
+		}).
+		error(function(data) {
+			// default
+			$scope.recipes = [
+				{'name': 'receipname1',
+				 'description': 'receipdescription1'},
+				{'name': 'receipname2',
+				 'description': 'receipdescription2'},
+			];
+		});
+	}]);
