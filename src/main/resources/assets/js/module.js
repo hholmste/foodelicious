a5fbcbeb88483bfb78e36a68e58871e604e420b5
Marius Breivik <marius.breivik@gmail.com>
2014-03-02 21:30:21 +0100

    Sigh

--- src/main/resources/assets/js/module.js
+++ src/main/resources/assets/js/module.js
@@ -7,10 +7,10 @@ recipeApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider
 			.when('/recipes', {
-				templateUrl: '/views/partials/recipe_overview.html'
+				templateUrl: 'views/partials/recipe_overview.html'
 			})
 			.when('/new', {
-				templateUrl: '/views/partials/recipe_detail.html'
+				templateUrl: 'views/partials/recipe_detail.html'
 			})
 			.otherwise({
 				redirectTo: '/recipes'
