87780e97cf84ab2c5a939bf9dcf6b929380cc4ca
Morten Berg <mortenberg80@gmail.com>
2014-03-11 11:36:32 +0100

    Test now checks if recipe accessed through GET is the same as the recipe sent to POST.

--- src/test/java/no/foodelicious/core/resources/RecipeResourceTest.java
+++ src/test/java/no/foodelicious/core/resources/RecipeResourceTest.java
@@ -1,13 +1,15 @@
 package no.foodelicious.core.resources;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
 import javax.ws.rs.core.MediaType;
 
 import no.foodelicious.core.configuration.MongoConfiguration;
 import no.foodelicious.core.factory.RepositoryFactory;
+import no.foodelicious.core.matchers.RecipeMatcher;
 import no.foodelicious.core.model.Recipe;
-import static org.fest.assertions.api.Assertions.assertThat;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.yammer.dropwizard.testing.ResourceTest;
@@ -16,19 +18,20 @@ public class RecipeResourceTest extends ResourceTest {
 
 	@Override
 	protected void setUpResources() throws Exception {
-		addResource(new RecipeResource(new RepositoryFactory().create(new MongoConfiguration())));
+		addResource(new RecipeResource(new RepositoryFactory().create(new MongoConfiguration("localhost", 27017))));
 	}
 
 	@Test
-	@Ignore
-	public void testCreateRecipe() {
-		Recipe recipe = new Recipe();
-		recipe.setName("Grandiosa");
-		recipe.setDescription("Sett på stekeovn");
-		recipe.setServings(1);
-		recipe.setSource("www.vg.no");
-		assertThat(client().resource("/recipe")
-				.type(MediaType.APPLICATION_JSON).post(Recipe.class, recipe)
-				.equals(recipe));
+	public void testCreateAndGetRecipe() {
+		Recipe originalRecipe = new Recipe();
+		originalRecipe.setName("Grandiosa");
+		originalRecipe.setDescription("Sett på stekeovn");
+		originalRecipe.setServings(1);
+		originalRecipe.setSource("www.vg.no");
+		Recipe createdRecipe = client().resource("/recipe").type(MediaType.APPLICATION_JSON).post(Recipe.class, originalRecipe);
+		assertThat(createdRecipe.getId()).isNotNull();
+
+		Recipe retrievedRecipe = client().resource("/recipe/" + createdRecipe.getId()).get(Recipe.class);
+		assertEquals(retrievedRecipe, RecipeMatcher.isEqualTo(originalRecipe));
 	}
 }
