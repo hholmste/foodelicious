a3d9485b2121f3f353b0feb6c3f3fa7efa411d8f
Hakon <harnengh@gmail.com>
2014-03-22 17:25:01 +0100

    Added serializer and deserializer for CourseType

--- src/main/java/no/foodelicious/core/model/CourseTypeDeserializer.java
+++ src/main/java/no/foodelicious/core/model/CourseTypeDeserializer.java
@@ -1,12 +1,25 @@
 package no.foodelicious.core.model;
 
-/**
- * Created with IntelliJ IDEA.
- * User: hakon
- * Date: 22/03/14
- * Time: 16:29
- * To change this template use File | Settings | File Templates.
- */
-public class CourseTypeDeserializer
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.*;
+
+import java.io.IOException;
+
+public class CourseTypeDeserializer extends JsonDeserializer<CourseType>
 {
+    @Override
+    public CourseType deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException
+    {
+        ObjectCodec oc = jsonParser.getCodec();
+        JsonNode node = oc.readTree(jsonParser);
+
+        if(node.get("code") != null) {
+            return CourseType.valueOf(node.get("code").asText());
+        }
+        return null;
+    }
+
 }
--- src/main/java/no/foodelicious/core/model/CourseTypeSerializer.java
+++ src/main/java/no/foodelicious/core/model/CourseTypeSerializer.java
@@ -1,24 +1,29 @@
 package no.foodelicious.core.model;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 
-public class CourseTypeSerializer extends JsonDeserializer<CourseType>
+public class CourseTypeSerializer extends JsonSerializer<CourseType>
 {
     @Override
-    public CourseType deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException
+    public void serialize(CourseType courseType, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException
     {
-        ObjectCodec oc = jsonParser.getCodec();
-        JsonNode node = oc.readTree(jsonParser);
-
-        System.out.printf("\nAAAAAAAAAAA\nThe node:\n%s\nAAAAAAAA\nthe node code\n%s\n", node.toString(), node.get("code"));
-
-        //return CourseType.valueOf(node.get("code").asText());
-        return  CourseType.MAIN_COURSE;
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeStringField("name", uncase(courseType.name()));
+        jsonGenerator.writeStringField("code", courseType.name());
+        jsonGenerator.writeEndObject();
     }
 
+    private String uncase(String allUpper) {
+        String allLower = allUpper.replace('_', ' ').toLowerCase();
+        return allLower.substring(0, 1).toUpperCase() + allLower.substring(1);
+    }
 }
